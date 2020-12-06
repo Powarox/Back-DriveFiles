314eb61e59432543364b9fe84a645c5c67b955f4
Powarox2159 <moriniere.robin1877@gmail.com>
2020-12-06 19:18:17 +0100

    Update

--- MoriniereRobinDev/index.php
+++ MoriniereRobinDev/index.php
@@ -7,14 +7,6 @@ require_once('Tools/Loader/Autoload.php');
 session_name("monSiteID"); //--> rename cookie PHPSESSID
 session_start();
 
-// function monAutoload($class){
-//     $arrayCheminClass = explode("\\", $class);
-//     //var_dump($arrayCheminClass);
-//     array_shift($arrayCheminClass);
-//     $stringCheminClass = implode('/', $arrayCheminClass);
-//     include($stringCheminClass . '.php');
-// }
-
 spl_autoload_register('MoriniereRobinDev\Tools\Loader\Autoload::monAutoload');
 
 //set_include_path("./MoriniereRobinDev");
